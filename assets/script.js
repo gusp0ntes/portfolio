2a83deb7a4a17739886b86dfed93a218b50b3e74
GustavoPontes <134552611+gusp0ntes@users.noreply.github.com>
2024-08-10 22:35:39 -0300

    Add files via upload

--- assets/script.js
+++ assets/script.js
@@ -1,11 +1,13 @@
+// Seleção de elementos do DOM
 let menuIcon = document.querySelector('#menu-icon');
 let navbar = document.querySelector('.navbar');
-let sections = document.querySelectorAll('section');
 let navlinks = document.querySelectorAll('header nav a');
 
+// Função para ativar link de navegação ao rolar a página
 window.onscroll = () => {
+    let top = window.scrollY;
+
     sections.forEach(sec => {
-        let top = window.scrollY;
         let offset = sec.offsetTop - 150;
         let height = sec.offsetHeight;
         let id = sec.getAttribute('id');
@@ -13,17 +15,19 @@ window.onscroll = () => {
         if (top >= offset && top < offset + height) {
             navlinks.forEach(link => {
                 link.classList.remove('active');
-                document.querySelector('header nav a[href*=' + id + ']').classList.add('active');
+                document.querySelector(`header nav a[href*="${id}"]`).classList.add('active');
             });
         }
     });
 };
 
+// Toggle para o ícone do menu e navbar
 menuIcon.onclick = () => {
     menuIcon.classList.toggle('bx-x');
     navbar.classList.toggle('active');
 };
 
+// Fechar navbar ao clicar em um link
 navlinks.forEach(link => {
     link.onclick = () => {
         menuIcon.classList.remove('bx-x');
