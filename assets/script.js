af69009550803d6c2f15f30f980f7128361d3466
GustavoPontes <134552611+gusp0ntes@users.noreply.github.com>
2024-08-10 22:47:31 -0300

    Add files via upload

--- assets/script.js
+++ assets/script.js
@@ -1,28 +1,26 @@
-// Seleção de elementos do DOM
-let menuIcon = document.querySelector('#menu-icon');
-let navbar = document.querySelector('.navbar');
-let navlinks = document.querySelectorAll('header nav a');
+const menuIcon = document.querySelector('#menu-icon');
+const navbar = document.querySelector('.navbar');
+const sections = document.querySelectorAll('section');
+const navlinks = document.querySelectorAll('header nav a');
 
-// Função para ativar link de navegação ao rolar a página
 window.onscroll = () => {
-    let top = window.scrollY;
+    const top = window.scrollY;
 
     sections.forEach(sec => {
-        let offset = sec.offsetTop - 150;
-        let height = sec.offsetHeight;
-        let id = sec.getAttribute('id');
+        const offset = sec.offsetTop - 150;
+        const height = sec.offsetHeight;
+        const id = sec.getAttribute('id');
 
         if (top >= offset && top < offset + height) {
-            navlinks.forEach(link => {
-                link.classList.remove('active');
-                document.querySelector(`header nav a[href*="${id}"]`).classList.add('active');
-            });
+            navlinks.forEach(link => link.classList.remove('active'));
+            document.querySelector(`header nav a[href*="${id}"]`).classList.add('active');
         }
     });
 };
 
 // Toggle para o ícone do menu e navbar
 menuIcon.onclick = () => {
+    menuIcon.classList.toggle('rotated');
     menuIcon.classList.toggle('bx-x');
     navbar.classList.toggle('active');
 };
@@ -30,6 +28,7 @@ menuIcon.onclick = () => {
 // Fechar navbar ao clicar em um link
 navlinks.forEach(link => {
     link.onclick = () => {
+        menuIcon.classList.remove('rotated');
         menuIcon.classList.remove('bx-x');
         navbar.classList.remove('active');
     };
